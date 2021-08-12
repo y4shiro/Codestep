55a5afcb458db105fe22bf1d3ced23330ae6d4fb
y4shiro <y4shironao@gmail.com>
2021-08-12 18:58:30 +0900

    section#reason のコーディング

--- lp-menu/js/main.js
+++ lp-menu/js/main.js
@@ -15,4 +15,36 @@ document.addEventListener('DOMContentLoaded', () => {
       navi.classList.remove('active');
     }
   };
+
+  // Inview
+  // ここは jQuery + プラグインを使用
+  // intersection observer で書き直せそう
+  $('.inview-slide-left').on(
+    'inview',
+    function (event, isInView, visiblePartX, visiblePartY) {
+      if (isInView) {
+        // 要素が表示されたらslide-leftクラスを追加
+        $(this).stop().addClass('slide-left');
+      }
+    }
+  );
+  $('.inview-slide-right').on(
+    'inview',
+    function (event, isInView, visiblePartX, visiblePartY) {
+      if (isInView) {
+        // 要素が表示されたらslide-rightクラスを追加
+        $(this).stop().addClass('slide-right');
+      }
+    }
+  );
+  // 受講生の声（ふきだし）
+  $('.inview-balloon').on(
+    'inview',
+    function (event, isInView, visiblePartX, visiblePartY) {
+      if (isInView) {
+        // 要素が表示されたらballoonクラスを追加
+        $(this).stop().addClass('balloon');
+      }
+    }
+  );
 });
